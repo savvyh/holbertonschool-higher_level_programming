a26832e86f9c50b9ed4d696ecf2aad3d63da0a6c
savvyh <7277@holbertonstudents.com>
2024-01-23 12:22:56 +0000

    fix the python program 1-last_digit.py by change the variable name (more tahn 80 caracters on the line) and adding a condition that check if the last digit is a negative number

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-l_digit = number % 10
+last = number % 10
 
-if l_digit > 5:
-    print(f"Last digit of {number} is {l_digit} and is greater than 5")
+if number < 0:
+    last = -(-number % 10)
+    if last == 0:
+        print(f"Last digit of {number} is {last} and is 0")
+    else:
+        print(f"Last digit of {number} is {last} and is less than 6 and not 0")
 
-elif l_digit == 0:
-    print(f"Last digit of {number} is {l_digit} and is 0")
+elif last > 5:
+    print(f"Last digit of {number} is {last} and is greater than 5")
 
-elif (l_digit < 6) and (l_digit != 0):
-    print(f"Last digit of {number} is {l_digit} and is less than 6 and not 0")
+elif last == 0:
+    print(f"Last digit of {number} is {last} and is 0")
+
+elif (last < 6) and (last != 0):
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
