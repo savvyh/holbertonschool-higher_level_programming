9531ab2749bd0e89a73c210cf4b44a2973af6163
savvyh <7277@holbertonstudents.com>
2024-01-24 14:11:08 +0000

    Fix the 0-add.py file by adding if __name__ == "__main__"

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-
-a = 1
-b = 2
-result = a + b
-print(f"{a} + {b} = {result}")
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
