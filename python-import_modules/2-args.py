2829f222d07ed80cae0a5d4332cc508e36106936
savvyh <7277@holbertonstudents.com>
2024-01-25 13:04:13 +0000

    Adding the line if __name__ == "__main__": in the file 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 from sys import argv
+if __name__ == "__main__":
 
-number_args = len(argv) - 1
-if number_args == 0:
-    print("{} arguments.".format(number_args))
+    number_args = len(argv) - 1
+    if number_args == 0:
+        print("{} arguments.".format(number_args))
 
-elif number_args == 1:
-    print("{} argument:" .format(number_args))
+    elif number_args == 1:
+        print("{} argument:" .format(number_args))
 
-else:
-    print("{} arguments :".format(number_args))
+    else:
+        print("{} arguments :".format(number_args))
 
-number = 0
-for index in argv:
-    if number != 0:
-        print("{}: {}".format(number, index))
-    number += 1
+    number = 0
+    for index in argv:
+        if number != 0:
+            print("{}: {}".format(number, index))
+        number += 1
