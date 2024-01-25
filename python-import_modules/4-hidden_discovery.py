df267d6af1a97d243bae9667b4e3e3a5ca289b15
savvyh <7277@holbertonstudents.com>
2024-01-25 14:50:12 +0000

    Fix the python program that prints all names defu=ine by a compiled module by adding an index

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    total_files = dir(hidden_4)
-    for index in total_files:
-        if index == '_':
-            continue
-    print("{}".format(index))
+    name_files = dir(hidden_4)
+    for index in name_files:
+        if index[0] != '_':
+            print("{}".format(index))
