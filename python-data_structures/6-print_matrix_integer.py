a85dbb1e5a210e3ac3e457824495cd26a3346211
savvyh <7277@holbertonstudents.com>
2024-01-30 12:19:30 +0000

    Python program that prints a matrix of integers

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for element in row:
+            if element == row[-1]:
+                print("{:d}".format(element), end="")
+            else:    
+                print("{:d} ".format(element), end="")
+        print()
