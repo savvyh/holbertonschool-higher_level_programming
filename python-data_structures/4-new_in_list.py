f3b406a3e68ceaf5afcc889f64d3c3d5854597b4
savvyh <7277@holbertonstudents.com>
2024-01-30 08:59:05 +0000

    Fix the python program that replaces an element in a list at a specific position without modifying the original list. I changed the return from the condition that checks if idx is not out of the range of the list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     copy_my_list = my_list.copy()
     if idx >= len(my_list) or idx < 0:
-        return copy_my_list[element]
+        return copy_my_list
 
     copy_my_list[idx] = element
     return copy_my_list
