e42b4c66daf7c0d7433b62ea9f5e48e2883ea85e
savvyh <7277@holbertonstudents.com>
2024-02-01 13:24:31 +0000

    Fix the python program that  prints the first x elements of a list and only integers, by change the order of the code and also change the except informations

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        number = 0
-        for i in range(x):
-            if type(my_list[i]) is int:
-                print("{:d}".format(my_list[i]), end="")
-                number += 1
-        print()
-    except IndexError:
-        pass
-        print()
+    number = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            number += 1
+        except (ValueError, TypeError):
+            pass
+    print()
     return number
