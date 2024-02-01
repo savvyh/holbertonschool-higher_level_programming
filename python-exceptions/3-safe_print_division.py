542c86ebb682517d3aee01cd53c4a1e0898ac110
savvyh <7277@holbertonstudents.com>
2024-02-01 13:51:27 +0000

    Fix pycodestyle in the file 3-safe_print_division.py by adding a new line at the end and erase a whitespace

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        result = None 
+        result = None
     finally:
         print("Inside result: {}".format(result))
 
