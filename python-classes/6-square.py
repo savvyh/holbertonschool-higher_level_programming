545469bc23dc301e1fed1043cb7efd9ab6eef135
savvyh <7277@holbertonstudents.com>
2024-02-06 11:04:28 +0000

    Fix pycodestyle of the file 6-square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -50,5 +50,5 @@ class Square:
         self.__position = value
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not all(isinstance(x, int) for x in value) or (x < 0 for x in value):
+        if not all(isinstance(x, int)for x in value) or (x < 0 for x in value):
             raise TypeError("position must be a tuple of 2 positive integers")
