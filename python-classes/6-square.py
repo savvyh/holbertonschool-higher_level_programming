4c82875917cd4acb3f45248ca6c94765e3434855
savvyh <7277@holbertonstudents.com>
2024-02-06 10:11:28 +0000

    Fix the python program that defines a square by adding position, and I just change the check of isinstance (change int to tuple, and check the length of value). I also modify the def my_print (self) to print a space

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -33,10 +33,10 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-        if self.__position[1] > 0:
+        for _ in range(self.__position[1]):
             print()
-        for i in range(self.__size):
-            print("#" * self.__size)
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
 
     """Property to get value of the position"""
     @property
@@ -47,5 +47,5 @@ class Square:
     @position.setter
     def position(self, value):
         self.__position = value
-        if not isinstance(value, int):
+        if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
