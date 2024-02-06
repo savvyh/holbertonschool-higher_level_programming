ea610e4be2265bf9092cdaa71fd942e7c711cfbf
savvyh <7277@holbertonstudents.com>
2024-02-06 15:05:20 +0000

    Fix the file 6-square.py by changinf the name of the variable in the loop for, in y function def my_print

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -19,11 +19,11 @@ class Square:
     """Property setter to set the size into value"""
     @size.setter
     def size(self, value):
+        self.__size = value
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
 
     """Property to get value of the position"""
     @property
@@ -33,21 +33,22 @@ class Square:
     """Property setter to set position into value & check if 2 + integers"""
     @position.setter
     def position(self, value):
+        self.__position = value
         if (
             not isinstance(value, tuple) or len(value) != 2
             or not all(isinstance(x, int) for x in value)
             or not all(x >= 0 for x in value)
         ):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
 
     """Prints the square with character #"""
     def my_print(self):
         if self.__size == 0:
             print()
-        for i in range(self.__position[1]):
+            return
+        for _ in range(self.__position[1]):
             print()
-        for j in range(self.__size):
+        for _ in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
 
     """Public instance method that return the current square area"""
