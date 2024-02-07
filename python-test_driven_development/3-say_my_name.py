eec63509958f2da7252dd661565d9e0e478a6137
savvyh <7277@holbertonstudents.com>
2024-02-07 17:27:13 +0000

    Fix the document description of the file 3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,10 +2,14 @@
 """
     Function that prints "my name is" follow by my first name and my last name.
     First name and last name must be string
+    Print Error message if not a string
 """
 
 
 def say_my_name(first_name, last_name=""):
+    """
+        Function that prints "My name is" follow by first + last name
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
