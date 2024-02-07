dd82c44b63536a5a4859113cfb1d3ee7149aa92d
savvyh <7277@holbertonstudents.com>
2024-02-07 17:36:54 +0000

    Fix the python program that adds 2 integers, modification of the return function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -15,4 +15,9 @@ def add_integer(a, b=98):
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    return int(a) + int(b)
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+
+    return a + b
