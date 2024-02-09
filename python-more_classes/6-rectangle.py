0f106b59b126432c2c3e360d326beee2f1b894df
savvyh <7277@holbertonstudents.com>
2024-02-09 11:11:27 +0000

    Fix the python program of the file 6-rectangle.py just by change the order of the code

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -7,12 +7,12 @@
 
 
 class Rectangle:
-
-    number_of_instances = 0
-
     """
         Initialize the function to create a rectangle
     """
+
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
