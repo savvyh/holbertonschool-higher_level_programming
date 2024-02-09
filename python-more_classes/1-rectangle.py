4abd86823e26e5f72cae6a01f5349a5f5ce9a59b
savvyh <7277@holbertonstudents.com>
2024-02-09 09:26:14 +0000

    Fix the python program and modify the def __init__

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
         Initialize the function to create a rectangle
     """
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     """
         Function that retrieves the height attribute
