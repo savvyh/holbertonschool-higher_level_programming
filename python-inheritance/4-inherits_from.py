13378b6b4809ab8c22f35fbe83263f6d9a7355f2
savvyh <7277@holbertonstudents.com>
2024-02-13 10:31:42 +0000

    Fix the documentation of the function inherits_from

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,5 @@
 
 
 def inherits_from(obj, a_class):
+    """ check if an instance is from a class or inherited a specific class """
     return isinstance(obj, a_class) and type(obj) is not a_class
