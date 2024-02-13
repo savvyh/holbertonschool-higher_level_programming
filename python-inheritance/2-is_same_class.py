f4f48da965bef283de88bb8bba46952a63ca1516
savvyh <7277@holbertonstudents.com>
2024-02-13 07:57:42 +0000

    Python program that check if the object is exactly an instance of a specific class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    Function that returns True if the object is exactly
+    an instance of the class
+"""
+
+
+def is_same_class(obj, a_class):
+    return type(obj) is a_class
