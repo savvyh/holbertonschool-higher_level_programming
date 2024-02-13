32587c9f21d0dc75a1234a972048541eec6b092c
savvyh <7277@holbertonstudents.com>
2024-02-13 11:40:44 +0000

    Python program that raises an exception if area is not implemented

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Write a class BaseGeometry based on file 5-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """
+        Public instance method.
+
+        Raise: Exception if area is not implemented
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
