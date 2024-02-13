95aaa3ecd952f5648dba8953ef5c7db8d4c0272b
savvyh <7277@holbertonstudents.com>
2024-02-13 08:52:15 +0000

    Fix the documentation of my function print_sorted

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-    Function that write a class MyList that inherits from list
+    Class MyList that inherits from list
 """
 
 
 class MyList(list):
+    """Function that sort a list inside a new list"""
     def print_sorted(self):
         sorted_list = sorted(self)
         print(sorted_list)
