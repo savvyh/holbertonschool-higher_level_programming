84943811cb0af25a43cf4dd6ea832da27d7148ab
savvyh <7277@holbertonstudents.com>
2024-02-14 13:29:55 +0000

    Fix the 10-square.py by delete the area method

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -54,7 +54,3 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        """implemented the area method"""
-        return (self.__size * self.__size)
