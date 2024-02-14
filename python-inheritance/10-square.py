9238d4573960a9fbd2c0ab43760195770c7ffffb
savvyh <7277@holbertonstudents.com>
2024-02-14 13:27:04 +0000

    Fix the python program of the file 10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -51,9 +51,10 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Args: size (_type_): _description_"""
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def area(self):
         """implemented the area method"""
-        return (self.__size ** 2)
+        return (self.__size * self.__size)
