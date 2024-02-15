07f947e44ca510515cb23198cea2940cfb48ad5a
savvyh <7277@holbertonstudents.com>
2024-02-15 12:59:17 +0000

    Fix the documentation of the function read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""function that reads a text file"""
+"""
+    function that reads a text file
+"""
 
 
 def read_file(filename=""):
+    """Args: filename (str, optional): _description_. Defaults to "".
+    """
     with open(filename, 'r', encoding="utf-8") as file:
         print(file.read(), end="")
