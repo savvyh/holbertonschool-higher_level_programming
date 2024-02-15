4591a51354c2def80f6bbe01e44f58ee97a90e8e
savvyh <7277@holbertonstudents.com>
2024-02-15 12:56:40 +0000

    Python program that reads a text file, using `with`

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function that reads a text file"""
+
+
+def read_file(filename=""):
+    with open(filename, 'r', encoding="utf-8") as file:
+        print(file.read(), end="")
