aa6ae55f1819223524a5db3040286a063bfded28
savvyh <7277@holbertonstudents.com>
2024-02-15 21:55:10 +0000

    Fix the python program that writes an object to a text file using JSON representation

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using a JSON representation"""
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(json.dumps(my_obj))
+        json.dump(my_obj, f)
