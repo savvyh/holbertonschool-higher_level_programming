962d7f3e26b61219c25710b6a18f13e12846abae
savvyh <7277@holbertonstudents.com>
2024-02-15 08:15:02 +0000

    Fix the file 8-rectangle.py by importing the class Rectangle of the function 7-base_geometry

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,26 +3,7 @@
     Write a class BaseGeometry based on file 7-base_geometry.py
 """
 
-
-class BaseGeometry:
-    """
-        Public instance method.
-
-        Raise: Exception if area is not implemented
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    """
-        Public instance method.
-
-        Raise: TypeError and ValueError
-    """
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
