fb72369be7072371d46d516653ebe5600760afc0
savvyh <7277@holbertonstudents.com>
2024-02-15 23:06:25 +0000

    Python program that replaces all attributes of the Student instance, using setattr

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""defines class Student"""
+
+
+class Student():
+    """class to define student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes student instances"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns dictionary description for JSON serialization"""
+        if isinstance(attrs, list):
+            json_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    json_dict[attr] = getattr(self, attr)
+            return json_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
