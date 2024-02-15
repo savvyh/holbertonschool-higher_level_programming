56e164a027c21f6d5e0ee7c1fc8ef4fcc4bfd1ae
savvyh <7277@holbertonstudents.com>
2024-02-15 22:50:41 +0000

    Fix pycodestyle of the file 9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,7 +4,7 @@
 
 class Student():
     """class to define student"""
-    
+
     def __init__(self, first_name, last_name, age):
         """initializes student instances"""
         self.first_name = first_name
