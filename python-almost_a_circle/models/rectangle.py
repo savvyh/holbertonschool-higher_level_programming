cdba5ed096e0fb776f21bb199f80b4b8c670f673
savvyh <7277@holbertonstudents.com>
2024-02-20 21:15:28 +0000

    Adding the function that return the area of a Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -16,10 +16,21 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Get the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        Set the width of the rectangle.
+
+        Args:
+            value (int): The width value to set.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value <= 0:
@@ -28,10 +39,21 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """Get the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        Set the height of the rectangle.
+
+        Args:
+            value (int): The height value to set.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value <= 0:
@@ -40,10 +62,21 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """Get the x of the top-left corner of the rectangle."""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """
+        Set the x of the top-left corner of the rectangle.
+
+        Args:
+            value (int): The x value to set.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         elif value < 0:
@@ -52,12 +85,27 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """Get the y of the top-left corner of the rectangle."""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """
+        Set the y of the top-left corner of the rectangle.
+
+        Args:
+            value (int): The y value to set.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         elif value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Return the area value of the Rectangle instance"""
+        return self.__width * self.__height
