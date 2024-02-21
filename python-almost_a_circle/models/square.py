5a0cb1ad01bc8fc2906735666ff2ae52bdcd999b
savvyh <7277@holbertonstudents.com>
2024-02-21 16:50:13 +0000

    Update the class Square by adding some validation for the size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -14,13 +14,26 @@ class Square(Rectangle):
     @property
     def size(self):
         """Get the size"""
-        return self.height
+        return self.width
 
     @size.setter
     def size(self, value):
-        """Set the value of size"""
-        self.height = value
+        """
+        Set the value of size
+
+        Args:
+            value (int): The width value to set.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0."""
         self.width = value
+        self.height = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
     def __str__(self):
         """Return a string with all attributes"""
