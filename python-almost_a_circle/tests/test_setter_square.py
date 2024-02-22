b288955b8f860968cb80dc1c3fcef99a2d2dd33a
savvyh <7277@holbertonstudents.com>
2024-02-22 07:48:31 +0000

    Fix the unittest file of that test the setter of the square

--- python-almost_a_circle/tests/test_setter_square.py
+++ python-almost_a_circle/tests/test_setter_square.py
@@ -24,5 +24,21 @@ class TestSquare(unittest.TestCase):
         with self.assertRaises(ValueError):
             Rectangle(-1, 2, 4, 5)
 
+    def test_size_setter_float(self):
+        """Test to set a float to the size"""
+        with self.assertRaises(TypeError):
+            Square(4, 3.14, 7, 1)
+
+    def test_size_setter_string(self):
+        """Test to set a string to the size"""
+        with self.assertRaises(TypeError):
+            Rectangle("hello", 2, 4, 5)
+
+    def test_size_setter_None(self):
+        """Test to set a None to the size"""
+        with self.assertRaises(TypeError):
+            Rectangle(None, 2, 4, 5)
+
+
 if __name__ == '__main__':
     unittest.main()
