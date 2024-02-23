a61520b3d381c2f18bd21ed179193d0dcf078f83
savvyh <7277@holbertonstudents.com>
2024-02-23 12:34:12 +0000

    Update the method create of the class Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -46,7 +46,10 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """Return an instance with all attributes already set"""
-        dummy = cls(1, 1)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
 
