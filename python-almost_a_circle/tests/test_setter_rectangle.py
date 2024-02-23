c6b4cd137b735524f32f244c66af3fd25fa212e1
savvyh <7277@holbertonstudents.com>
2024-02-23 14:35:22 +0000

    Update the unittest of that test the height and the width of the rectangle

--- python-almost_a_circle/tests/test_setter_rectangle.py
+++ python-almost_a_circle/tests/test_setter_rectangle.py
@@ -17,51 +17,69 @@ class TestSetterRectangle(unittest.TestCase):
         """Test the property of the height"""
         rect = Rectangle(2, 3, 8, 7, 1)
         self.assertEqual(3, rect.height)
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 2, 3, 5, 6)
+        with self.assertRaises(TypeError):
+            Rectangle(3.14, 5, 7, 8, 1)
+        with self.assertRaises(TypeError):
+            Rectangle("hello", 5, 8, 7, 9)
+        with self.assertRaises(TypeError):
+            Rectangle(2, [1], 3, 4, 5)
+        with self.assertRaises(TypeError):
+            Rectangle(2, {52}, 3, 4, 5)
+        with self.assertRaises(TypeError):
+            Rectangle(3, (1,2), 4, 5, 6)
 
     def test_width_getter(self):
         """Test the property of the width"""
         rect = Rectangle(2, 7, 1, 3, 9)
         self.assertEqual(2, rect.width)
+        with self.assertRaises(ValueError):
+            Rectangle(1, -2, 3, 4, 5)
+        with self.assertRaises(TypeError):
+            Rectangle(4, 3.14, 7, 8, 1)
+        with self.assertRaises(TypeError):
+            Rectangle(6, "hello", 8, 7, 9)
+        with self.assertRaises(TypeError):
+            Rectangle([1], 2, 3, 4, 5)
+        with self.assertRaises(TypeError):
+            Rectangle({52}, 2, 3, 4, 5)
+        with self.assertRaises(TypeError):
+            Rectangle((1,2), 3, 4, 5, 6)
 
     def test_height_setter(self):
         """Test the setter of the height"""
         rect = Rectangle(1, 12, 3, 15, 6)
         self.assertEqual(12, rect.height)
+        with self.assertRaises(ValueError):
+            Rectangle(1, -2, 3, 4, 5)
+        with self.assertRaises(TypeError):
+            Rectangle(4, 3.14, 7, 8, 1)
+        with self.assertRaises(TypeError):
+            Rectangle(6, "hello", 8, 7, 9)
+        with self.assertRaises(TypeError):
+            Rectangle(2, [1], 3, 4, 5)
+        with self.assertRaises(TypeError):
+            Rectangle(2, {52}, 3, 4, 5)
+        with self.assertRaises(TypeError):
+            Rectangle(3, (1,2), 4, 5, 6)
 
     def test_width_setter(self):
         """Test the setter of the width"""
         rect = Rectangle(4, 3, 2, 1, 9)
         self.assertEqual(4, rect.width)
-
-    def test_height_setter_negative(self):
-        """Test to set a negative int to the height"""
-        with self.assertRaises(ValueError):
-            Rectangle(1, -2, 3, 4, 5)
-
-    def test_width_setter_negative(self):
-        """Test to set a negative int to the width"""
         with self.assertRaises(ValueError):
             Rectangle(-1, 2, 3, 4, 5)
-
-    def test_height_setter_float(self):
-        """Test to set a float to the height"""
-        with self.assertRaises(TypeError):
-            Rectangle(4, 3.14, 7, 8, 1)
-
-    def test_width_setter_float(self):
-        """Test to set a float to the width"""
         with self.assertRaises(TypeError):
             Rectangle(3.14, 6, 7, 8, 1)
-
-    def test_height_setter_string(self):
-        """Test to set a string to the height"""
-        with self.assertRaises(TypeError):
-            Rectangle(6, "hello", 8, 7, 9)
-
-    def test_width_setter_string(self):
-        """Test to set a string to the width"""
         with self.assertRaises(TypeError):
             Rectangle("hello", 5, 8, 7, 9)
+        with self.assertRaises(TypeError):
+            Rectangle([1], 2, 3, 4, 5)
+        with self.assertRaises(TypeError):
+            Rectangle({52}, 2, 3, 4, 5)
+        with self.assertRaises(TypeError):
+            Rectangle((1,2), 3, 4, 5, 6)
 
 
 if __name__ == '__main__':
