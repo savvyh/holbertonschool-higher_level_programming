282cc706ad4b03ff10a5f7690e2af15a4059cbca
savvyh <7277@holbertonstudents.com>
2024-02-23 13:04:40 +0000

    Update the unittest file that test the json_string to and from

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -12,51 +12,58 @@ class TestBase(unittest.TestCase):
         """Reset the variable __nb_objects for each test"""
         Base._Base__nb_objects = 0
 
-    def test_id_is_None(self):
-        """Test with id is None"""
+    def test_without_id(self):
+        """Test without id"""
         test1 = Base()
         self.assertEqual(test1.id, 1)
-        test2 = Base()
-        self.assertEqual(test2.id, 2)
-        test3 = Base()
-        self.assertEqual(test3.id, 3)
 
-    def test_id_is_not_None(self):
-        """Test with id=value"""
-        test1 = Base()
-        self.assertEqual(test1.id, 1)
-        test2 = Base(5)
-        self.assertEqual(test2.id, 5)
-        test3 = Base(15)
-        self.assertEqual(test3.id, 15)
-        test4 = Base()
-        self.assertEqual(test4.id, 2)
+    def test_with_id(self):
+        """Test with a specific ID"""
+        test2 = Base(12)
+        self.assertEqual(test2.id, 12)
 
-    def test_id_is_Zero(self):
-        """Test with id is equal to Zero"""
-        test1 = Base()
-        self.assertEqual(test1.id, 1)
-        test2 = Base(0)
-        self.assertEqual(test2.id, 0)
-        test3 = Base()
-        self.assertEqual(test3.id, 2)
-        test4 = Base()
-        self.assertEqual(test4.id, 3)
+    def test_with_negative_id(self):
+        """Test with a negative ID"""
+        test3 = Base(-8)
+        self.assertEqual(test3.id, -8)
 
-    def test_id_is_positive(self):
-        """Test if ID is positive"""
-        test = Base(id=1)
-        self.assertTrue(test.id > 0)
+    def test_with_float_id(self):
+        """Test with a float ID"""
+        test4 = Base(6.3)
+        self.assertEqual(test4.id, 6.3)
 
-    def test_id_is_negative(self):
-        """Test if ID is negative"""
-        test = Base(id=-1)
-        self.assertTrue(test.id < 0)
+    def test_without_id_after_others(self):
+        """Test without id (after other instantiations)"""
+        test5 = Base()
+        self.assertEqual(test5.id, 1)
 
-    def test_id_is_integer(self):
-        """Test if ID is an integer"""
-        test = Base(id="string")
-        self.assertIsInstance(test.id, str)
+    def test_with_none_id(self):
+        """Test with None as ID"""
+        test6 = Base(None)
+        self.assertEqual(test6.id, 1)
+
+    def test_to_json_string_empty(self):
+        """Test to_json_string with empty list"""
+        result = Base.to_json_string([])
+        self.assertEqual(result, "[]")
+
+    def test_to_json_string_non_empty(self):
+        """Test to_json_string with non-empty list"""
+        input_list = [{"key": "value"}, {"key2": "value2"}]
+        result = Base.to_json_string(input_list)
+        self.assertEqual(result, '[{"key": "value"}, {"key2": "value2"}]')
+
+    def test_from_json_string_empty(self):
+        """Test from_json_string with empty string"""
+        result = Base.from_json_string("")
+        self.assertEqual(result, [])
+
+    def test_from_json_string_non_empty(self):
+        """Test from_json_string with non-empty string"""
+        input_string = '[{"key": "value"}, {"key2": "value2"}]'
+        result = Base.from_json_string(input_string)
+        expected_result = [{"key": "value"}, {"key2": "value2"}]
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == '__main__':
