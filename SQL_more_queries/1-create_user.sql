68c1f3dac708ea66f1141d5541c31edf52b93ac6
savvyh <7277@holbertonstudents.com>
2024-03-20 07:53:17 +0000

    Fix the code by adding a ;

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create user in the MySQL server, user should have all privileges.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
