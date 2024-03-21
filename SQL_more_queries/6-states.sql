ef9f9972a6dcb2c5eb925955baa998437bb19ff9
savvyh <7277@holbertonstudents.com>
2024-03-21 09:29:09 +0000

    Script that create a new database and a new_table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Create a new database and a new_table.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL,
+    NAME VARCHAR(256) NOT NULL,
+    PRIMARY KEY(id)
+);
