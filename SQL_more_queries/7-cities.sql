1406a0e0c265ddcd191af8d2db2e7b5a0875c2e8
savvyh <7277@holbertonstudents.com>
2024-03-21 09:35:36 +0000

    Create a new table inside the last database created. And use a foreign key refere to the table states

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Create a new table inside the last database created.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL,
+    name VARCHAR(256),
+    state_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+);
