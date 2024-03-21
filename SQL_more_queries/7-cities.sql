bd606285b20e299ab05fcfc8c8c82995c011d546
savvyh <7277@holbertonstudents.com>
2024-03-21 09:41:08 +0000

    Fix the code

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,8 @@
 -- Create a new table inside the last database created.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-    id INT AUTO_INCREMENT UNIQUE NOT NULL,
-    name VARCHAR(256) NOT NULL,
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
+    name VARCHAR(256) NOT NULL
 );
