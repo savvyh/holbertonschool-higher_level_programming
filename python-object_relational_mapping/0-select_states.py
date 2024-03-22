ae4bde960868a68d1707fbebad21a8ab52ab5708
savvyh <7277@holbertonstudents.com>
2024-03-22 14:42:00 +0000

    try with a new code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,38 +1,16 @@
 #!/usr/bin/python3
 """
-A Python script that lists all states from the database hbtn_0e_0_usa.
-Script takes 3 arguments: mysql username, mysql password, and database name.
-Script uses the MySQLdb module to connect to a MySQL server
-running on localhost at port 3306.
-Results are sorted in ascending order by states.id.
+Python script that lists all states from the database hbtn_0e_0_usa
 """
-
+from sys import argv
 import MySQLdb
-import sys
-
-
-def list_states(username, password, db_name):
-    """
-    Connects to a MySQL database and lists all states from the database.
-    """
-    db = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        passwd=password,
-        db=db_name,
-        port=3306
-    )
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cur.fetchall()
-
-    for state in states:
-        print(state)
-
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
     cur.close()
     db.close()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) == 4:  # Ensuring there are three arguments
-        list_states(sys.argv[1], sys.argv[2], sys.argv[3])
