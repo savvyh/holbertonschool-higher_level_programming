89e7a11ff9218f0f80a39b4fe812d7704b6140b7
savvyh <7277@holbertonstudents.com>
2024-03-22 14:35:16 +0000

    Fix the code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """
 A Python script that lists all states from the database hbtn_0e_0_usa.
+Script takes 3 arguments: mysql username, mysql password, and database name.
+Script uses the MySQLdb module to connect to a MySQL server
+running on localhost at port 3306.
+Results are sorted in ascending order by states.id.
 """
 
 import MySQLdb
@@ -18,14 +22,14 @@ def list_states(username, password, db_name):
         db=db_name,
         port=3306
     )
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cursor.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cur.fetchall()
 
     for state in states:
         print(state)
 
-    cursor.close()
+    cur.close()
     db.close()
 
 
